dadef9323cc02bebfec628b87d2e22252fa934cf
Kamil Piękoś <k.piekos@gbxsoft.com>
2021-05-04 17:14:28 +0200

    fixed auto login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
+import { LoggedInGuard } from './core/guards/logged-in.guard';
 
 
 const routes: Routes = [
   {
     path: 'auth',
+    canActivate: [LoggedInGuard],
     loadChildren: () => import('./modules/authorization/authorization.module').then(m => m.AuthorizationModule)
   },
   {
